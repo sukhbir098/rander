f446082e57d78f41c9e1d77702f2ca49ff48c0ea
Sukhbhir Mundlia <sukhbirmundlia90@gmail.com>
2025-03-25 11:56:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express();
+const POST = process.env.POST || 3000
+
+app.get("/",(req,res)=>{
+    res.send("Hello Rander..")
+})
+
+
+
+app.listen(PORT,(req,res)=>{
+console.log(`Server Runnint at ${PORT}`)
+})
