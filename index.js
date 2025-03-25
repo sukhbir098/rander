ff71160d1aa993a306a34a4e70ea8380b50e2563
Sukhbhir Mundlia <sukhbirmundlia90@gmail.com>
2025-03-25 13:01:34 +0530

    new

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose"); // Fixed the typo
 const app = express();
-const PORT = process.env.POST || 3000
 
-app.get("/",(req,res)=>{
-    res.send("Hello Rander..")
-})
+const PORT = process.env.PORT || 3000; 
 
+const DB_URI = 'mongodb+srv://sukhbirmundlia:sachin123@cluster0.3b0qq.mongodb.net/testing';
+app.use(express.json());
 
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-app.listen(PORT,(req,res)=>{
-console.log(`Server Runnint at ${PORT}`)
-})
+app.get("/", (req, res) => {
+    res.send("Hello Render..");
+});
+
+app.post("/data", (req, res) => {
+    const requestData = req.body; // Fixed variable name
+    console.log('Received Data:', requestData);
+    res.json({ message: 'Data received successfully', data: requestData });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Running at ${PORT}`);
+});
